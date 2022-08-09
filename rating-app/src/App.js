c9fc3021463acf23c4ff722b152d734ab140408b
Zeynep Gültekin - dotSource GmbH <zgu@intranet.dotsource.de>
2022-08-09 12:14:50 +0200

    add active class

--- rating-app/src/App.js
+++ rating-app/src/App.js
@@ -5,30 +5,39 @@ import Thanks from './component/Thanks';
 function App() {
   const [isShown, setIsShown] = useState(true);
   const [rating, setRating] = useState('');
+  const [active, setActive] = useState('');
   const handleClick = () => {
     setIsShown(false)
   }
   const handleRating = (e) => {
-  setRating(e.target.innerHTML);
+    setRating(e.target.innerHTML);
+    setActive(+e.target.innerHTML)
   }
 
   return (
     <div className='container'>
+    
       <div className='card__rate card' style={{display: isShown ? 'initial' : 'none'}}>
+      {/* <div className='card__rate card'> */}
         <img src={star} alt="star for rating" className='card__rate__img'/>
         <h1 className="card__title">How did we do?</h1>
         <p className="card__text">Please let us know how we did with your support request. All feedback is appreciated 
     to help us improve our offering!</p>
         <ul className="card__rate__list">
           { Array.from(Array(5), (_, index) => index + 1).map((number, key) =>{
-           return ( <li key={key}><button className='card__rate__btn' onClick={handleRating}>{number}</button></li>)
+           return ( <li key={key}><button 
+            className={`card__rate__btn ${active === number && 'active'}`}
+            onClick={handleRating}>{number}</button></li>)
           })}
         </ul>
         <button onClick={handleClick} type="button" className="card__sbmt">Submit</button>
       </div>
+      
       <Thanks isShown={isShown} rating={rating} />
+       
     </div>
   );
 }
 
 export default App;
+
